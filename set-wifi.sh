ba95997b93f23cb1587ae2a8076e9147ae7970f1
Norberto Rodriguez <norberto1976@hotmail.com>
2022-05-18 16:43:51 -0300

    Error en SSID por el nombre del wifi por espacio en blanco y la ruta iot por norberto

--- set-wifi.sh
+++ set-wifi.sh
@@ -1,7 +1,7 @@
 SSID=$( grep "^#define WIFI_SSID"  ../config/config.h | rev | cut -d'"' -f 2 | rev )
 PASSWORD=$( grep "^#define WIFI_PASSWORD"  ../config/config.h | rev | cut -d'"' -f 2 | rev )
 
-sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="'${SSID}'"/' 
-sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="'${PASSWORD}'"/' 
-
-
+sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="Fibertel WiFi153 2.4GHz"/'
+#sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="'${SSID}'"/'
+sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="34250596356318"/'
+#sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="'${PASSWORD}'"/'
