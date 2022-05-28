7f358e32fe81e56239a8ba7f4982aa9a3e40f778
Norberto Rodriguez <norberto1976@hotmail.com>
2022-05-28 19:12:25 -0300

    quito una linea enn wifi

--- set-wifi.sh
+++ set-wifi.sh
@@ -3,5 +3,4 @@ PASSWORD=$( grep "^#define WIFI_PASSWORD"  ../config/config.h | rev | cut -d'"' 
 
 sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="Fibertel WiFi153 2.4GHz"/'
 #sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="'${SSID}'"/'
-sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="34250596356318"/'
-#sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="'${PASSWORD}'"/'
+sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="'${PASSWORD}'"/'
